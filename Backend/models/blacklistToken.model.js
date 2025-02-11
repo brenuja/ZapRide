a79b9eab1256296a630122a7e84bc935a7b4f6ed
B Renuja <156912989+brenuja@users.noreply.github.com>
2025-02-11 21:30:36 +0530

    Add files via upload

---
+++ Backend/models/blacklistToken.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const blacklistTokenSchema = new mongoose.Schema({
+    token: {
+        type: String,
+        required: true,
+        unique: true 
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 86400 // TTL of 24 hours (in seconds)
+    }
+});
+
+// Create and export the model
+module.exports = mongoose.model('BlacklistToken', blacklistTokenSchema);
+
